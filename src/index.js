646f64cf80133986c96eb089a0abc58447972a6c
Haiyang Hong <hong961127@gmail.com>
2020-11-23 21:22:10 -0800

    Implement refresh token

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const qs = require("query-string");
 const axios = require("axios");
 const session = require("express-session");
-const path = require("path");
+const NodeCache = require("node-cache");
 
+// Env vars
 const client_id = process.env.HB_CLIENT_ID;
 const client_secret = process.env.HB_CLIENT_SECRET;
 const redirect_uri = process.env.HB_REDIRECT_URI;
 const installUrl = process.env.HB_INSTALL_URL;
+
+// Use node cache to manage short lived access token
+const nodeCache = new NodeCache();
 
 const app = express();
 app.set("view engine", "pug");
@@ -16,23 +21,58 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(
   session({
-    secret: Math.random().toString(36).substring(2),
+    secret: Math.random().toString(36).substring(2), // dcgio3ppss
     resave: false,
     saveUninitialized: true,
   })
 );
 
-// Mock token store and auth check function
-const tokenStore = {};
+// Mock refresh token DB
+const refreshTokenStore = {};
 const isAuthenticated = (userId) => {
-  return !!tokenStore[userId];
+  return !!refreshTokenStore[userId];
+};
+
+// Returns user's access token from cache if exists
+// Otherwise get a new one using refresh token
+const getAccessToken = async (userId) => {
+  try {
+    if (nodeCache.get(userId)) {
+      console.log("🔥 ----> Cached access token used.");
+      return nodeCache.get(userId);
+    }
+
+    console.log("🔥 ----> Requested new access token using refresh token.");
+
+    const refreshParams = {
+      grant_type: "refresh_token",
+      client_id,
+      client_secret,
+      refresh_token: refreshTokenStore[userId],
+    };
+
+    const { data } = await axios.post(
+      `https://api.hubapi.com/oauth/v1/token`,
+      qs.stringify(refreshParams)
+    );
+
+    // Stores refresh token into DB
+    refreshTokenStore[userId] = data.refresh_token;
+
+    // Store access token into node cache with a TTL that matches access token expiry
+    nodeCache.set(userId, data.access_token, 10);
+
+    return data.access_token;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Conditionally render views based on whether use is authenticated
 app.get("/", async (req, res, next) => {
   try {
     if (isAuthenticated(req.sessionID)) {
-      const accessToken = tokenStore[req.sessionID];
+      const accessToken = await getAccessToken(req.sessionID);
 
       // Attach access token as bearer token
       const headers = {
@@ -53,7 +93,7 @@ app.get("/", async (req, res, next) => {
       res.render("home", { authUrl: installUrl });
     }
   } catch (error) {
-    console.error(error);
+    console.error(error.response);
   }
 });
 
@@ -77,12 +117,16 @@ app.get("/oauth-callback", async (req, res, next) => {
       qs.stringify(authProof) // qs helps convert object into url-encoded format
     );
 
-    // Stores access token into store
-    tokenStore[req.sessionID] = data.access_token;
+    // Stores refresh token into DB
+    refreshTokenStore[req.sessionID] = data.refresh_token;
 
-    res.redirect("/"); // Redirects back to home route
+    // Store access token into node cache with a TTL that matches access token expiry
+    nodeCache.set(req.sessionID, data.access_token, 10);
+
+    // Redirects back to home route
+    res.redirect("/");
   } catch (error) {
-    console.error(error);
+    console.error(error.response);
   }
 });
 
